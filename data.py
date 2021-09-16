8151d55aafc33bacda1cee65e5718dc96e771486
matttorrez <57408698+matttorrez@users.noreply.github.com>
2021-09-15 21:37:37 -0700

    Update data.py

--- data.py
+++ data.py
@@ -16,6 +16,7 @@ d = datetime.date(2021,9,14)
 end = str(time.mktime(d.timetuple()))[:-2]
 #Id say try using GMT
 end = '1631563200'
+print(time.ctime(int(end)))
 ticker = 'AAPL'
 APIkey= 'c4rrfr2ad3ic8b7crong'
 url = 'https://finnhub.io/api/v1/stock/candle?symbol='+ticker+'&resolution='+interval+'&from='+start+'&to='+end+'&token='+APIkey
@@ -23,3 +24,14 @@ with urllib.request.urlopen(url) as response:
    data = response.read()
 data = data.decode('utf-8')
 data = json.loads(data)
+for i in range(len(data['t'])):
+    data['t'][i] = datetime.datetime.strptime(time.ctime(data['t'][i])[4:],'%b  %d %H:%M:%S %Y')
+for i in reversed(data['t']):
+    if not(i > i.replace(hour=6,minute=30) and i < i.replace(hour=13,minute=0)):
+        index = data['t'].index(i)
+        for item in data:
+            try:
+                data[item].remove(data[item][index])
+            except:
+                print('I tried')
+            
